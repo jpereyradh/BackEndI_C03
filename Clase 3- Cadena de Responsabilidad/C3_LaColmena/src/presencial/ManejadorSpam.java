975619cca403a2d21bd985c302b6c9bdc3ad6099
jpereyradh <jorge.pereyra@digitalhouse.com>
2024-05-02 20:04:54 -0300

    C3

---
+++ Clase 3- Cadena de Responsabilidad/C3_LaColmena/src/presencial/ManejadorSpam.java
@@ -0,0 +1,9 @@
+package presencial;
+
+public class ManejadorSpam extends Manejador{
+    @Override
+    public String comprobarMail(Mail mail) {
+        System.out.println("finalmente no se pudo procesar la solicitud");
+        return "movido a spam";
+    }
+}
